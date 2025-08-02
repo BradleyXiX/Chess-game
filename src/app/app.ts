505ef765bd43d8e36ad79bcc42438467d8560b3d
Bradley <157209409+BradleyXiX@users.noreply.github.com>
2025-08-02 16:19:15 +0300

    creating abstract pieces classes and every piece class, chess board basics props and component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChessBoardComponent } from "./modules/chess-board/chess-board";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ChessBoardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
